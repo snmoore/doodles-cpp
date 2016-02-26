e185fe76c95cb322f15019f3cdfa445fa24e82e0
Simon Moore <simon@snmoore.net>
2016-02-26 16:25:28 +0000

    Added a reinterpret_cast

--- easy_steps/cast.cpp
+++ easy_steps/cast.cpp
@@ -6,17 +6,21 @@ int main() {
     int num = 7, factor = 2;
     char letter = 'A';
     float result = 0.0;
+    void* data;
 
     cout << "Integer division: " << (num / factor) << endl;
 
     result = ((float)num)/factor;
     cout << "Cast float division: " << result << endl;
 
-    num = static_cast <int>(letter);
+    num = static_cast<int>(letter);
     cout << "Cast char to int: " << num << endl;
 
-    letter = static_cast <char>(70);
+    letter = static_cast<char>(70);
     cout << "Cast int to char: " << letter << endl;
+
+    data = reinterpret_cast<void*>(num);
+    cout << "Cast int to void*: " << data << endl;
 
     return 0;
 }
