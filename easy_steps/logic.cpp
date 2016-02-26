516f615acc8e1370c7881c1b372b7ea9843e5ae1
Simon Moore <simon@snmoore.net>
2016-02-26 15:41:45 +0000

    Tweak the output to be easier to follow

--- easy_steps/logic.cpp
+++ easy_steps/logic.cpp
@@ -5,23 +5,23 @@ using namespace std;
 int main() {
     int a = 1, b = 0;
 
-    cout << "Boolean AND: ";
-    cout << "a && a: " << (a && a) << " (true) ";
-    cout << "a && b: " << (a && b) << " (false) ";
-    cout << "b && b: " << (b && b) << " (false) " << endl;
+    cout << "Boolean AND: " << endl;
+    cout << "1 && 1: " << (a && a) << " (true)"  << endl;
+    cout << "1 && 0: " << (a && b) << " (false)" << endl;
+    cout << "0 && 0: " << (b && b) << " (false)" << endl;
 
-    cout << "Boolean OR: ";
-    cout << "a || a: " << (a || a) << " (true) ";
-    cout << "a || b: " << (a || b) << " (true) ";
-    cout << "b || b: " << (b || b) << " (false) " << endl;
+    cout << "Boolean OR: " << endl;
+    cout << "1 || 1: " << (a || a) << " (true)"  << endl;
+    cout << "1 || 0: " << (a || b) << " (true)"  << endl;
+    cout << "0 || 0: " << (b || b) << " (false)" << endl;
 
-    cout << "Boolean XOR: ";
-    cout << "a ^ a: " << (a ^ a) << " (false) ";
-    cout << "a ^ b: " << (a ^ b) << " (true) ";
-    cout << "b ^ b: " << (b ^ b) << " (false) " << endl;
+    cout << "Boolean XOR: " << endl;
+    cout << "1 ^ 1: " << (a ^ a) << " (false)" << endl;
+    cout << "1 ^ 0: " << (a ^ b) << " (true)"  << endl;
+    cout << "0 ^ 0: " << (b ^ b) << " (false)" << endl;
 
-    cout << "Boolean NOT: ";
-    cout << "a = " << a << " !a = " << !a << " ";
+    cout << "Boolean NOT: " << endl;
+    cout << "a = " << a << " !a = " << !a << endl;
     cout << "b = " << a << " !b = " << !b << endl;
 
     return 0;
