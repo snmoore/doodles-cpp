3db1a6bd5597982e3bcdce27a65544974a370993
Simon Moore <simon@snmoore.net>
2016-02-26 15:30:04 +0000

    Add some subscripting examples

--- easy_steps/vector.cpp
+++ easy_steps/vector.cpp
@@ -4,23 +4,38 @@
 using namespace std;
 
 int main() {
-    vector <int> vec(3, 100);
+    vector<int> vec(3, 100);
 
-    cout << "Vector size: " << vec.size() << endl;
-    cout << "Is empty?: " << vec.empty() << endl;
-    cout << "First element: " << vec.front() << endl;
+    cout << "Vector size:     " << vec.size() << endl;
+    cout << "Vector capacity: " << vec.capacity() << endl;
+    cout << "Is empty?:       " << vec.empty() << endl;
+    cout << "First element:   " << vec.front() << endl;
+    cout << endl;
 
     vec.pop_back(); // remove the final element
-    cout << "Vector size: " << vec.size() << endl;
-    cout << "Final element: " << vec.back() << endl;
+    cout << "Vector size:     " << vec.size() << endl;
+    cout << "Final element:   " << vec.back() << endl;
+    cout << endl;
 
     vec.clear();    // remove all elements
-    cout << "Vector size: " << vec.size() << endl;
-    cout << "Is empty?: " << vec.empty() << endl;
+    cout << "Vector size:     " << vec.size() << endl;
+    cout << "Vector capacity: " << vec.capacity() << endl;
+    cout << "Is empty?:       " << vec.empty() << endl;
+    cout << endl;
 
     vec.push_back(200); // add an element
-    cout << "Vector size: " << vec.size() << endl;
-    cout << "First element: " << vec.front() << endl;
+    cout << "Vector size:     " << vec.size() << endl;
+    cout << "Vector capacity: " << vec.capacity() << endl;
+    cout << "First element:   " << vec.front() << endl;
+    cout << endl;
+
+    vec[0] = 25; // change an element, without range checking
+    cout << "vec[0]:          " << vec[0] << endl;
+    cout << endl;
+
+    vec.at(0) = 50; // change an element, with range checking
+    cout << "vec.at(0):       " << vec.at(0) << endl;
+    cout << endl;
 
     return 0;
 }
