ae507bd2e512d25f9bcd27b05b4412d3d77e71e0
Simon Moore <simon@snmoore.net>
2016-02-26 16:31:53 +0000

    Adjust whitespace to make it easier to read

--- easy_steps/switch.cpp
+++ easy_steps/switch.cpp
@@ -6,12 +6,12 @@ int main() {
     int num = 6;
 
     switch(num) {
-    case 1: cout << num << " : Monday" << endl; break;
-    case 2: cout << num << " : Tuesday" << endl; break;
-    case 3: cout << num << " : Wednesday" << endl; break;
-    case 4: cout << num << " : Thursday" << endl; break;
-    case 5: cout << num << " : Friday" << endl; break;
-    default: cout << num << " : Weekend" << endl; break;
+    case 1:  cout << num << " : Monday"    << endl; break;
+    case 2:  cout << num << " : Tuesday"   << endl; break;
+    case 3:  cout << num << " : Wednesday" << endl; break;
+    case 4:  cout << num << " : Thursday"  << endl; break;
+    case 5:  cout << num << " : Friday"    << endl; break;
+    default: cout << num << " : Weekend"   << endl; break;
     }
 
     return 0;
