d89c8ecb9c5eb80aec88341459cc3b875aa48f10
Simon Moore <simon@snmoore.net>
2016-03-04 10:56:09 +0000

    Re-written for practice

--- count_bits/count_bits.cpp
+++ count_bits/count_bits.cpp
@@ -2,44 +2,73 @@
 //
 // See https://graphics.stanford.edu/~seander/bithacks.html
 
-#include <iostream>     // For cout etc
+#include <bitset>   // For std::bitset
+#include <iomanip>  // For std::setw
+#include <iostream> // For std::cout etc
+#include <string>   // For std::string
 
-using namespace std;
+#define NELEMENTS(array)    ((sizeof(array))/(sizeof(array[0])))
 
-// Count using the obvious method; 32 iterations
-int obvious(unsigned int v) {
-    int bits;
-    for(bits = 0; v; v >>= 1) {
-//        if(v & 1) {
-//            bits++;
-//        }
-        bits += v & 1;
+// Obvious implementation to count the number of bits, up to 32 iterations
+unsigned int obvious(unsigned int value) {
+    // Check each bit and count
+    unsigned int bits {0};
+    for(; value; value >>= 1) {
+        bits += value & 1;
     }
     return bits;
 }
 
-// Count using Kernighan's method; as many iterations as there are bits set
-int kernighan(unsigned int v) {
-    int bits;
-    for(bits = 0; v; bits++) {
-        v &= v - 1; // clear the least significant bit
+// Obvious implementation with shortcut, up to 32 iterations
+unsigned int shortcut(unsigned int value) {
+    // Shortcut on powers-of-2
+    if((value & (value - 1)) == 0) {    // is value a power-of-2?
+        return value != 0;              // 0 is not really a power-of-2
     }
 
+    // Check each bit and count
+    unsigned int bits {0};
+    for(; value; value >>= 1) {
+        bits += value & 1;
+    }
     return bits;
 }
 
-int main(void) {
-    unsigned int values[] = { 0, 1, 2, 3, 4, 5, 7, 8, 10, 64, 128, 255 };
-
-    // Count the number of bits set using the obvious method
-    cout << "Obvious method:" << endl;
-    for(auto v : values) {
-        cout << "Value: " << v << " Bits: " << obvious(v) << endl;
+// Kernighan's method, only as many iterations as there are bits set
+unsigned int kernighan(unsigned int value) {
+    // Count bits whilst clearing the LSB
+    unsigned int bits {0};
+    for(; value; bits++) {
+        value &= value - 1;
     }
+    return bits;
+}
 
-    // Count the number of bits set using Kernighan's method
-    cout << "Kernighan's method:" << endl;
-    for(auto v : values) {
-        cout << "Value: " << v << " Bits: " << kernighan(v) << endl;
+// Test a given implementation
+void test(unsigned int (*function)(unsigned int value), std::string label) {
+    const unsigned int tests[] = {
+            0, 1, 2, 3, 4, 5, 7, 8, 9, 10,
+            8, 16, 32, 64, 128, 256, 512,
+            0xFF, 0xFFFF, 0xFFFFFF, 0xFFFFFFFF
+    };
+
+    std::cout << label << ":" << std::endl;
+    for(size_t i = 0; i < NELEMENTS(tests); i++) {
+        std::cout << std::setw(10) << std::showbase << std::hex << tests[i];
+        std::cout << std::setw(34) << std::bitset<32>(tests[i]);
+        std::cout << std::setw(4) << std::dec << function(tests[i]);
+        std::cout << std::endl;
     }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Test the obvious implementation
+    test(obvious, "Obvious");
+
+    // Test the obvious implementation with shortcut
+    test(shortcut, "Obvious with shortcut");
+
+    // Test Kernighan's method
+    test(kernighan, "Kernighan's Method");
 }
