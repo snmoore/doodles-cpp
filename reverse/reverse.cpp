7a7db24dd1f2a2a2c479aa4f2bf161ffabac62b5
Simon Moore <simon@snmoore.net>
2016-03-04 15:04:44 +0000

    Rewritten for practice

--- reverse/reverse.cpp
+++ reverse/reverse.cpp
@@ -1,93 +1,99 @@
-// Reverse words and sentences
-
-#include <algorithm>    // For reverse
+#include <algorithm>    // For std::reverse
 #include <cctype>       // For isspace
-#include <iostream>     // For cout etc
-#include <sstream>      // For stringstream
-#include <string>       // For string
-#include <vector>       // For vector
+#include <iostream>     // For std:cout etc
+#include <sstream>      // For std::stringstream
+#include <stack>        // For std::stack
+#include <string>       // For std::string
+#include <utility>      // For std::swap
 
-using namespace std;
-
-// Reverse a word using swap
-string word_swap(string s) {
-    for(size_t left = 0, right = s.size() - 1; left < right; left++, right--) {
-        swap(s[left], s[right]);
+// Reverse a word in place, using a temporary variable
+std::string& word_inplace_temp(std::string& word) {
+    size_t left;
+    size_t right;
+    char temp;
+    for(left = 0, right = word.size() - 1; left < right; left++, right--) {
+        temp        = word[right];
+        word[right] = word[left];
+        word[left]  = temp;
     }
-    return s;
+
+    return word; // allows for reversed = word_inplace_temp(word);
 }
 
-// Reverse a word using an iterator
-string word_iterator(string s) {
-    // Note: could just use auto rather than string::iterator
-    string reversed;
-    for(string::iterator iter = s.end(); iter != s.begin(); iter--) {
+// Reverse a word in place, using XOR
+std::string& word_inplace_xor(std::string& word) {
+    size_t left;
+    size_t right;
+    for(left = 0, right = word.size() - 1; left < right; left++, right--) {
+        word[right] ^= word[left];
+        word[left]  ^= word[right];
+        word[right] ^= word[left];
+    }
+
+    return word; // allows for reversed = word_inplace_xor(word);
+}
+
+// Reverse a word in place, using pointers
+std::string& word_inplace_pointer(std::string& word) {
+    char* left;
+    char* right;
+    char temp;
+    for(left = &word[0], right = left + word.size() - 1; left < right; left++, right--) {
+        temp   = *right;
+        *right = *left;
+        *left  = temp;
+    }
+
+    return word; // allows for reversed = word_inplace_pointer(word);
+}
+
+// Reverse a word in place, using swap
+std::string& word_inplace_swap(std::string& word) {
+    size_t left;
+    size_t right;
+    for(left = 0, right = word.size() - 1; left < right; left++, right--) {
+        std::swap(word[left], word[right]);
+    }
+
+    return word; // allows for reversed = word_inplace_swap(word);
+}
+
+// Reverse a word in place, using reverse
+std::string& word_inplace_reverse(std::string& word) {
+    std::reverse(word.begin(), word.end());
+
+    return word; // allows for reversed = word_inplace_reverse(word);
+}
+
+// Reverse a const word, using an iterator
+std::string word_const_iterator(const std::string& word) {
+    std::string reversed {""};
+    for(auto iter = word.rbegin(); iter != word.rend(); iter++) {
         reversed += *iter;
     }
-    reversed += *s.begin();
     return reversed;
 }
 
-// Reverse a word using a reverse iterator
-string word_riterator(string s) {
-    // Note: could just use auto rather than string::iterator
-    string reversed;
-    for(string::reverse_iterator iter = s.rbegin(); iter != s.rend(); iter++) {
-        reversed += *iter;
-    }
+// Reverse a const word, using reverse
+std::string word_const_reverse(const std::string& word) {
+    std::string reversed {word};
+    std::reverse(reversed.begin(), reversed.end());
     return reversed;
 }
 
-// Reverse a word using algorithm
-string word_algorithm(string s) {
-    reverse(s.begin(), s.end());
-    return s;
-}
-string& word_algorithm_in_place(string& s) {
-    reverse(s.begin(), s.end());
-    return s;
-}
-
-// Reverse the order of words in a sentence by splitting into words and recombining
-string sentence_split(string s) {
-    // Split the sentence into words and add to a vector
-    stringstream stream(s);
-    vector<string> words {};
-    while(stream) {
-        string word {};
-        stream >> word;
-        words.push_back(word);
-    }
-
-    // Reverse the vector
-    reverse(words.begin(), words.end());
-
-    // Recombine into a single string
-    string reversed;
-    for(auto word : words) {
-        reversed += word + " ";
-    }
-
-    return reversed;
-}
-
-// Reverse the order of words in a sentence by looping
-string sentence_loop(string s) {
-    // Find each word in the sentence and reverse it
-    int start = -1;
-    for(size_t i = 0; i < s.size(); i++) {
-        if(isspace(s[i])) {
-            if(start == -1) {
-                // In the middle of whitespace
-                continue;
-            }
-            else {
+// Reverse the order of words in a sentence, in place
+std::string& sentence_inplace(std::string& sentence) {
+    // Reverse each word in the sentence
+    int start {-1};
+    for(size_t i = 0; i < sentence.size(); i++) {
+        if(isspace(sentence[i])) {
+            if(start != -1) {
                 // Found the end of a word, reverse it
-                string word = s.substr(start, i - start);
-                string rword = word_algorithm_in_place(word);
-                s.replace(start, i - start, rword);
+                std::string word = sentence.substr(start, i - start);
+                word_inplace_reverse(word);
+                sentence.replace(start, i - start, word);
 
-                // Look for a new word
+                // Look for the next word
                 start = -1;
             }
         }
@@ -96,43 +102,109 @@ string sentence_loop(string s) {
                 // Found the start of a word
                 start = i;
             }
-            else {
-                // In the middle of a word
-                continue;
-            }
         }
     }
 
     // Reverse the final word
     if(start != -1) {
-        string word = s.substr(start, s.size() - start);
-        string rword = word_algorithm_in_place(word);
-        s.replace(start, s.size() - start, rword);
+        std::string word = sentence.substr(start, sentence.size() - 1);
+        word_inplace_reverse(word);
+        sentence.replace(start, sentence.size() - 1, word);
     }
 
-    // Reverse all of the characters in the sentence
-    return word_algorithm(s);
+    // Reverse the entire text
+    word_inplace_reverse(sentence);
+
+    return sentence;
+}
+
+// Reverse the order of words in a const sentence, using insert
+std::string sentence_const_insert(const std::string& sentence) {
+    std::string reversed {};
+
+    // Extract each word via a stringstream and prepend to a string
+    std::stringstream stream(sentence);
+    while(!stream.eof()) {
+        std::string word {};
+        stream >> word;
+        reversed.insert(0, word);
+        reversed.insert(0, " ");
+    }
+
+    // Remove the extraneous space at the front of the string
+    reversed.erase(0, 1);
+
+    return reversed;
+}
+
+// Reverse the order of words in a const sentence, using a stack
+std::string sentence_const_stack(const std::string& sentence) {
+    std::string reversed {};
+
+    // Extract each word via a stringstream and push onto a stack
+    std::stack<std::string> words {};
+    std::stringstream stream(sentence);
+    while(stream) {
+        std::string word {};
+        stream >> word;
+        words.push(word);
+    }
+
+    // Build the reversed sentence from the stack
+    while(!words.empty()) {
+        reversed += words.top();
+        reversed += " ";
+        words.pop();
+    }
+
+    // Remove the extraneous space at the front of the string
+    reversed.erase(0, 1);
+
+    return reversed;
+}
+
+// Test a given implementation
+void test(std::string& (*function)(std::string& text), std::string& text, const std::string& label) {
+    std::cout << label << ":" << std::endl;
+    std::cout << text << " --> " << function(text) << std::endl;
+    std::cout << std::endl;
+}
+void test(std::string (*function)(const std::string& word), const std::string& text, const std::string& label) {
+    std::cout << label << ":" << std::endl;
+    std::cout << text << " --> " << function(text) << std::endl;
+    std::cout << std::endl;
+}
+
+// Test reversing individual words
+void test_words() {
+    // Test reversing words in place
+    std::string word {"Hello"};
+    test(word_inplace_temp,    word, "Reverse a word in place, using a temporary variable");
+    test(word_inplace_xor,     word, "Reverse a word in place, using XOR");
+    test(word_inplace_pointer, word, "Reverse a word in place, using pointers");
+    test(word_inplace_swap,    word, "Reverse a word in place, using swap");
+    test(word_inplace_reverse, word, "Reverse a word in place, using reverse");
+
+    // Test reversing const words
+    test(word_const_iterator, "World", "Reverse a const word, using an iterator");
+    test(word_const_reverse,  "World", "Reverse a const word, using reverse");
+}
+
+// Test reversing sentences
+void test_sentences() {
+    // Test reversing sentences in place
+    std::string sentence {"Reverse the order of words in a sentence"};
+    test(sentence_inplace, sentence, "Reverse the order of words in a sentence, in place");
+
+    // Test reversing const sentences
+    test(sentence_const_insert, "Reverse the order of words in a sentence", "Reverse the order of words in a const sentence, using insert");
+    test(sentence_const_stack,  "Reverse the order of words in a sentence", "Reverse the order of words in a const sentence, using a stack");
 }
 
 int main() {
-    string word     = "Hello";
-    string sentence = "Reverse the order of words in a sentence";
+    // Test reversing individual words
+    test_words();
 
-    cout << "Reverse a word using swap:" << endl;
-    cout << word << " -> " << word_swap(word) << endl << endl;
-
-    cout << "Reverse a word using an iterator:" << endl;
-    cout << word << " -> " << word_iterator(word) << endl << endl;
-
-    cout << "Reverse a word using a reverse iterator:" << endl;
-    cout << word << " -> " << word_riterator(word) << endl << endl;
-
-    cout << "Reverse a word using algorithm:" << endl;
-    cout << word << " -> " << word_algorithm(word) << endl << endl;
-
-    cout << "Reverse the order of words in a sentence by splitting into words and recombining:" << endl;
-    cout << sentence << " -> " << sentence_split(sentence) << endl << endl;
-
-    cout << "Reverse the order of words in a sentence by looping:" << endl;
-    cout << sentence << " -> " << sentence_loop(sentence) << endl << endl;
+    // Test reversing sentences
+    test_sentences();
 }
