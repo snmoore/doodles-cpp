7882d7d763d1bfa16719843e5c36bcf52042cb5c
Simon Moore <simon@snmoore.net>
2016-03-04 10:58:48 +0000

    Rewritten for practice

--- roman_numerals/roman_numerals.cpp
+++ roman_numerals/roman_numerals.cpp
@@ -11,166 +11,171 @@
 //  left-to-right : need the current and next two numerals to decide whether to
 //                  add or subtract
 
-#include <iomanip>      // For setw
-#include <iostream>     // For cout etc
-#include <map>          // For map
+#include <iomanip>      // For std:setw
+#include <iostream>     // For std::cout etc
+#include <map>          // For std::map
+#include <stdexcept>    // For std::invalid_argument
+#include <string>       // For std::string
 
-using namespace std;
-
-// Roman numerals
+// A trivial class dealing with Roman numerals
 class Roman {
-private:
-    string str;
-    unsigned int char_to_decimal(char numeral) const; // convert a single Roman numeral to decimal
-
 public:
-    Roman(string str) :str(str) {}
-    Roman(unsigned int decimal);        // construct a Roman numeral from a decimal value
-    unsigned int getDecimal() const;    // get the decimal representation of the Roman numerals
-    string getString() const;           // get the string representation of the Roman numerals
+    Roman(std::string roman);
+    Roman(unsigned int decimal);
+
+    std::string string() const;
+    unsigned int decimal() const;
+
+private:
+    std::string str;
+
+    unsigned int string2decimal(std::string roman) const;
+    std::string decimal2string(unsigned int decimal) const;
 };
 
-// Convert a single Roman numeral to decimal
-unsigned int Roman::char_to_decimal(char numeral) const {
-  map<char,unsigned int> numerals {
-      { 'I', 1    },
-      { 'V', 5    },
-      { 'X', 10   },
-      { 'L', 50   },
-      { 'C', 100  },
-      { 'D', 500  },
-      { 'M', 1000 },
-  };
-
-  // Using at() - throws out_of_range if the key was not found
-  return numerals.at(numeral);
-
-  // Using find() - returns iter.end() if the key was not found
-#if 0
-  auto iter = numerals.find(numeral);
-  if(iter == numerals.end()) {
-      throw runtime_error("Invalid Roman numeral: " + string(1, numeral) + "\n");
-  }
-  return iter->second;
-#endif
-
-  // Using [] - this inserts a new (key,value) pair if the key was not found
-#if 0
-  unsigned int result = numerals[numeral];
-  if(result == 0) {
-      throw runtime_error("Invalid Roman numeral: " + string(1, numeral) + "\n");
-  }
-  return result;
-#endif
+// Construct a Roman numeral from a string
+Roman::Roman(std::string roman) : str("") {
+    // Validate the string by converting to decimal and back again
+    str = decimal2string(string2decimal(roman));
 }
 
-// Construct a Roman numeral from a decimal value
-Roman::Roman(unsigned int decimal) {
-    if(decimal == 0) {
-        throw runtime_error("0 cannot be represented in Roman numerals");
-    }
+// Construct a Roman numeral from a decimal number
+Roman::Roman(unsigned int decimal) : str("") {
+    str = decimal2string(decimal);
+}
 
-    // Note that map is ordered, so listing M first has no benefit
-    map<unsigned int,char> numerals {
-        { 1,    'I' },
-        { 5,    'V' },
-        { 10,   'X' },
-        { 50,   'L' },
-        { 100,  'C' },
-        { 500,  'D' },
-        { 1000, 'M' },
+// Get a string representation of the Roman numeral
+std::string Roman::string() const {
+    return str;
+}
+
+// Get a decimal representation of the Roman numeral
+unsigned int Roman::decimal() const {
+    return string2decimal(str);
+}
+
+// Convert a Roman numeral to a decimal number
+unsigned int Roman::string2decimal(std::string roman) const {
+    const std::map<char, unsigned int> numerals {
+        { 'I',    1 },
+        { 'V',    5 },
+        { 'X',   10 },
+        { 'L',   50 },
+        { 'C',  100 },
+        { 'D',  500 },
+        { 'M', 1000 }
     };
 
-    // Work from M to I, subtracting numeral values whilst building the string
+    // Check the input
+    if(roman == "") {
+        throw(std::invalid_argument("string2decimal: empty string"));
+    }
+
+    // Work backwards through the string
+    unsigned int decimal {0};
+    unsigned int curr    {0}; // value of the current character
+    unsigned int prev    {0}; // value of the previous character
+    for(auto iter = roman.rbegin(); iter != roman.rend(); iter++) {
+        curr = numerals.at(*iter);
+
+        // Additive notation
+        if(curr >= prev) {
+            decimal += curr;
+        }
+        // Subtractive notation
+        else {
+            // Ratio of prev to curr should be 10 or less
+            if((curr * 10) < prev) {
+                throw(std::invalid_argument("string2decimal: invalid combination (ratio)"));
+            }
+
+            // VX, VL, LC, LD and DM are invalid
+            if((curr == 5) || (curr == 50) || (curr == 500)) {
+                throw(std::invalid_argument("string2decimal: invalid combination (fives)"));
+            }
+
+            decimal -= curr;
+        }
+
+        // Next iteration
+        prev = curr;
+    }
+    return decimal;
+}
+
+// Convert a decimal number to a Roman numeral
+std::string Roman::decimal2string(unsigned int decimal) const {
+    const std::map<unsigned int, std::string> numerals {
+        {    1, "I" },
+        {    5, "V" },
+        {   10, "X" },
+        {   50, "L" },
+        {  100, "C" },
+        {  500, "D" },
+        { 1000, "M" },
+    };
+
+    // Check the input
+    if(decimal == 0) {
+        throw(std::invalid_argument("decimal2string: no Roman numeral for zero"));
+    }
+
+    // Repeatedly subtract values, working from largest to smallest
+    std::string roman {""};
     for(auto iter = numerals.rbegin(); iter != numerals.rend() && decimal > 0; iter++) {
-        // Use the current numeral as many times as possible
-        while(iter->first <= decimal) {
-            str     += iter->second;
+        // Additive notation - use the current numeral as many times as possible
+        while(decimal >= iter->first) {
+            roman   += iter->second;
             decimal -= iter->first;
         }
 
         // Try subtractive notation before moving to the next lower numeral...
 
         // Subtractive notation using non-adjacent numerals e.g. IX, XC, CM
-        if((distance(iter, numerals.rend()) > 2) &&
-           ((iter->first != 50)  && (iter->first != 500)) && // VL and LD are not valid
-           (decimal >= iter->first - next(iter, 2)->first)) {
-            str += next(iter, 2)->second;
-            str += iter->second;
-            decimal -= iter->first - next(iter, 2)->first;
+        if((std::distance(iter, numerals.rend()) > 2) &&
+           (iter->second != "D") && (iter->second != "L") && // VL and LD are invalid
+           (decimal >= (iter->first - next(iter, 2)->first))) {
+            roman   += next(iter, 2)->second;
+            roman   += iter->second;
+            decimal -= (iter->first - next(iter, 2)->first);
         }
-        // Subtractive notation using adjacent numerals e.g. IV, XL or CD
-        else if((distance(iter, numerals.rend()) > 1) &&
-                ((iter->first / next(iter, 1)->first) == 5) && // VX, LC and DM are not valid
-                (decimal >= iter->first - next(iter, 1)->first)) {
-            str += next(iter, 1)->second;
-            str += iter->second;
-            decimal -= iter->first - next(iter, 1)->first;
+
+        // Subtractive notation using adjacent numerals e.g. IV, XL, CD
+        if((std::distance(iter, numerals.rend()) > 1) &&
+           (iter->second != "X") && (iter->second != "C") && (iter->second != "M") && // VX, LC and DM are invalid
+           (decimal >= (iter->first - next(iter, 1)->first))) {
+            roman   += next(iter, 1)->second;
+            roman   += iter->second;
+            decimal -= (iter->first - next(iter, 1)->first);
         }
     }
-}
-
-// Get the decimal representation of the Roman numerals
-unsigned int Roman::getDecimal() const {
-    unsigned int curr = 0;
-    unsigned int prev = 0;
-    unsigned int sum  = 0;
-    for(auto c = str.rbegin(); c != str.rend(); c++) {
-        curr = char_to_decimal(*c);
-        if(curr >= prev) {
-            // E.g. XI => curr=10, prev=1 => 10+1 => 11
-            sum += curr;
-        }
-        else {
-            // Vn, Ln and Dn are all invalid combinations
-            if(curr == 5 || curr == 50 || curr == 500) {
-                throw runtime_error("invalid combination");
-            }
-
-            // Ratio of prev to curr should be 10 or less
-            if((curr * 10) < prev) {
-                throw runtime_error("invalid combination");
-            }
-
-            // E.g. IX => curr=1, prev=10 => 10-1 => 9
-            sum -= curr;
-        }
-
-        // Next iteration
-        prev = curr;
-    }
-
-    return sum;
-}
-
-// Get the string representation of the Roman numerals
-string Roman::getString() const {
-    return str;
+    return roman;
 }
 
 // Common test function
-void test(string str, unsigned int decimal) {
-    // Convert in both directions
-    string d2s {};
-    unsigned int s2d {};
+void test(std::string roman, unsigned int decimal) {
+    std::cout << std::setw(13) << roman << " : ";
     try {
-        d2s = Roman(decimal).getString();
-        s2d = Roman(str).getDecimal();
-        if((s2d == decimal) && (d2s == str)) {
-            cout << setw(13) << d2s << " " << s2d << endl;
+        // Convert decimal number to Roman numeral and vice-versa
+        Roman d2r(decimal);
+        Roman r2d(roman);
+        if((d2r.string() == roman) && (d2r.decimal() == decimal) &&
+           (r2d.string() == roman) && (r2d.decimal() == decimal)) {
+            std::cout << decimal;
         }
         else {
-            cout << setw(13) << str << " - bad conversion, should be " << d2s << endl;
+            std::cout << "bad conversion"; // this should never occur
         }
+        std::cout << std::endl;
     }
-    catch(runtime_error& e) {
-        cout << setw(13) << str << " - bad conversion, should be " << d2s << endl;
+    catch(std::exception& e) {
+        std::cout << e.what() << std::endl;
     }
 }
 
 int main() {
     // Individual numerals
-    cout << "Individual numerals:" << endl;
+    std::cout << "Individual numerals:" << std::endl;
     test("I", 1);
     test("V", 5);
     test("X", 10);
@@ -178,20 +183,20 @@ int main() {
     test("C", 100);
     test("D", 500);
     test("M", 1000);
-    cout << endl;
+    std::cout << std::endl;
 
     // Valid combinations for subtractive notation
-    cout << "Valid combinations for subtractive notation:" << endl;
+    std::cout << "Valid combinations for subtractive notation:" << std::endl;
     test("IV", 4);
     test("IX", 9);
     test("XL", 40);
     test("XC", 90);
     test("CD", 400);
     test("CM", 900);
-    cout << endl;
+    std::cout << std::endl;
 
     // Numbers 1 to 10
-    cout << "Numbers 1 to 10:" << endl;
+    std::cout << "Numbers 1 to 10:" << std::endl;
     test("I",    1);
     test("II",   2);
     test("III",  3);
@@ -202,19 +207,24 @@ int main() {
     test("VIII", 8);
     test("IX",   9);
     test("X",    10);
-    cout << endl;
+    std::cout << std::endl;
 
     // Various years
-    cout << "Various years:" << endl;
+    std::cout << "Various years:" << std::endl;
     test("MDCCCLXXXVIII", 1888); // a very long year
     test("MCMIV",         1904); // from Wikipedia
     test("MCMLIV",        1954); // from Wikipedia - as in the trailer for the movie The Last Time I Saw Paris
     test("MCMXC",         1990); // from Wikipedia - used as the title of musical project Enigma's debut album MCMXC a.D., named after the year of its release.
     test("MMXIV",         2014); // from Wikipedia - the year of the games of the XXII (22nd) Olympic Winter Games (in Sochi)
-    printf("\n");
+    std::cout << std::endl;
+
+    // Invalid numbers
+    std::cout << "Invalid numbers:" << std::endl;
+    test("", 0);    // no Roman numeral to represent 0
+    std::cout << std::endl;
 
     // Invalid combinations for subtractive notation
-    cout << "Invalid combinations for subtractive notation:" << endl;
+    std::cout << "Invalid combinations for subtractive notation:" << std::endl;
     test("IL", 49);  // should be XLIX
     test("IC", 99);  // should be XCIX
     test("ID", 499); // should be CDXCIX
@@ -230,10 +240,10 @@ int main() {
     test("LD", 450); // should be CDL
     test("LM", 950); // should be CML
     test("DM", 500); // should be D
-    cout << endl;
+    std::cout << std::endl;
 
     // Valid counterparts to the invalid combinations
-    cout << "Valid counterparts to the invalid combinations:" << endl;
+    std::cout << "Valid counterparts to the invalid combinations:" << std::endl;
     test("XLIX",   49);
     test("XCIX",   99);
     test("CDXCIX", 499);
@@ -249,5 +259,5 @@ int main() {
     test("CDL",    450);
     test("CML",    950);
     test("D",      500);
-    cout << endl;
+    std::cout << std::endl;
 }
