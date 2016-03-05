af959b270b61addd620065ec81eb65372f9ec409
Simon Moore <simon@snmoore.net>
2016-03-05 10:33:41 +0000

    Rewritten for practice

--- binary_search/binary_search.cpp
+++ binary_search/binary_search.cpp
@@ -12,101 +12,112 @@
 //
 // See https://en.wikipedia.org/wiki/Binary_search_algorithm
 
-#include <algorithm>    // For binary_search, find
-#include <iostream>     // For cout etc
-#include <vector>       // For vector
-
-using namespace std;
+#include <algorithm>    // For std::binary_search
+#include <iostream>     // For std::cout etc
+#include <vector>       // For std::vector
 
 const int NOT_FOUND {-1};
 
-// Recursive binary search
-int recursive(vector<int>& values, int key, int lower, int upper) {
-    if(lower > upper) {
-        // The key was not found
-        return NOT_FOUND;
-    }
-
-    // Split the set into 2
-    int midpoint = (lower + upper) / 2;
-
-    if(key == values[midpoint]) {
-        // Found the key
-        return midpoint;
-    }
-    else if(key < values[midpoint]) {
-        // Continue searching in the lower-half
-        return recursive(values, key, lower, midpoint - 1);
-    }
-    else {
-        // Continue searching in the upper-half
-        return recursive(values, key, midpoint + 1, upper);
-    }
-}
-
-// Iterative binary search
-int iterative(vector<int>& values, int key) {
-    size_t lower {0};
-    size_t upper {values.size() - 1};
+// Iterative binary search, returns index where key == values[index]
+long int iterative(const std::vector<int>& values, const int key) {
+    unsigned long int lower {0};
+    unsigned long int upper {values.size() - 1};
 
     while(lower <= upper) {
-        // Split the set into 2
-        int midpoint = (lower + upper) / 2;
+        // Calculatr the midpoint
+        unsigned long int midpoint = (lower + upper) / 2;
 
+        // Is the key at the midpoint?
         if(key == values[midpoint]) {
             // Found the key
-            return midpoint;
+            return static_cast<int>(midpoint);
         }
+        // Is the key within the lower half range?
         else if(key < values[midpoint]) {
-            // Continue searching in the lower-half
+            // Keep searching in the lower half
             upper = midpoint - 1;
         }
+        // The key is within the upper half range
         else {
-            // Continue searching in the upper-half
+            // Keep searching in the upper half
             lower = midpoint + 1;
         }
     }
 
-    // The key was not found
+    // The key is not present
     return NOT_FOUND;
 }
 
-// Built-in find (may not be a binary search!)
-//
-// std::binary_search() returns if a key is present or not, not its index
-// std::find() returns an iterator to the first element if present
-// std::distance() allows to find the index by comparing to the original vector
-int builtin_find(vector<int>& values, int key) {
-    auto iter = find(values.begin(), values.end(), key);
-
-    if(iter != values.end()) {
-        return distance(values.begin(),iter);
-    }
-    else {
+// Recursive binary search, returns index where key == values[index]
+int recursive(const std::vector<int>& values, const int key, unsigned int lower, unsigned int upper) {
+    // Is the key not present?
+    if(lower > upper) {
         return NOT_FOUND;
+    }
+
+    // Find the midpoint
+    unsigned int midpoint = (lower + upper) / 2;
+
+    // Is the key at the midpoint?
+    if(key == values[midpoint]) {
+        // Found the key
+        return static_cast<int>(midpoint);
+    }
+    // Is the key within the lower half range?
+    else if(key < values[midpoint]) {
+        // Keep searching in the lower half
+        return recursive(values, key, lower, midpoint - 1);
+    }
+    // The key is within the upper half range
+    else {
+        // Keep searching in the upper half
+        return recursive(values, key, midpoint + 1, upper);
     }
 }
 
-int main(void) {
-    vector<int> values { 1, 2, 3, 4, 5, 7, 8, 9, 11, 13, 16, 17, 23, 27, 29, 32, 31, 37, 64, 81 };
+// Use std::find, returns index where key == values[index]
+//
+// Beware that this may not be implemented as a binary search
+int builtin_find(const std::vector<int>& values, const int key) {
+    // Find returns an iterator to the appropriate element
+    auto iter = std::find(values.begin(), values.end(), key);
+    if(iter == values.end()) {
+        return NOT_FOUND;
+    }
+    else {
+        // Distance allows us to calculate the index
+        return distance(iter, values.begin());
+    }
+}
 
-    cout << "Recursive:  1 ==> " << recursive(values,  1, 0, values.size() - 1) << endl;
-    cout << "Recursive:  3 ==> " << recursive(values,  3, 0, values.size() - 1) << endl;
-    cout << "Recursive: 14 ==> " << recursive(values, 14, 0, values.size() - 1) << endl;
-    cout << "Recursive: 81 ==> " << recursive(values, 81, 0, values.size() - 1) << endl;
-    cout << endl;
-    cout << "Iterative:  1 ==> " << iterative(values,  1) << endl;
-    cout << "Iterative:  3 ==> " << iterative(values,  3) << endl;
-    cout << "Iterative: 14 ==> " << iterative(values, 14) << endl;
-    cout << "Iterative: 81 ==> " << iterative(values, 81) << endl;
-    cout << endl;
-    cout << "Built-in binary_search:   1 ==> " << boolalpha << binary_search(values.begin(), values.end(),  1) << endl;
-    cout << "Built-in binary_search:   3 ==> " << boolalpha << binary_search(values.begin(), values.end(),  3) << endl;
-    cout << "Built-in binary_search:  14 ==> " << boolalpha << binary_search(values.begin(), values.end(), 14) << endl;
-    cout << "Built-in binary_search:  81 ==> " << boolalpha << binary_search(values.begin(), values.end(), 81) << endl;
-    cout << endl;
-    cout << "Built-in find:  1 ==> " << builtin_find(values,  1) << endl;
-    cout << "Built-in find:  3 ==> " << builtin_find(values,  3) << endl;
-    cout << "Built-in find: 14 ==> " << builtin_find(values, 14) << endl;
-    cout << "Built-in find: 81 ==> " << builtin_find(values, 81) << endl;
+int main() {
+    std::vector<int> values { 1, 2, 3, 4, 5, 7, 8, 9, 11, 13, 16, 17, 23, 27, 29, 32, 31, 37, 64, 81 };
+
+    // Test the iterative implementation
+    std::cout << "Iterative:  1 ==> " << iterative(values,  1) << std::endl;
+    std::cout << "Iterative:  3 ==> " << iterative(values,  3) << std::endl;
+    std::cout << "Iterative: 14 ==> " << iterative(values, 14) << std::endl;
+    std::cout << "Iterative: 81 ==> " << iterative(values, 81) << std::endl;
+    std::cout << std::endl;
+
+    // Test the recursive implementation
+    std::cout << "Recursive:  1 ==> " << recursive(values,  1, 0, values.size() - 1) << std::endl;
+    std::cout << "Recursive:  3 ==> " << recursive(values,  3, 0, values.size() - 1) << std::endl;
+    std::cout << "Recursive: 14 ==> " << recursive(values, 14, 0, values.size() - 1) << std::endl;
+    std::cout << "Recursive: 81 ==> " << recursive(values, 81, 0, values.size() - 1) << std::endl;
+    std::cout << std::endl;
+
+    // Test std::binary_search
+    std::cout << "std::binary_search:  1 ==> " << std::boolalpha << std::binary_search(values.begin(), values.end(),  1) << std::endl;
+    std::cout << "std::binary_search:  3 ==> " << std::boolalpha << std::binary_search(values.begin(), values.end(),  3) << std::endl;
+    std::cout << "std::binary_search: 14 ==> " << std::boolalpha << std::binary_search(values.begin(), values.end(), 14) << std::endl;
+    std::cout << "std::binary_search: 81 ==> " << std::boolalpha << std::binary_search(values.begin(), values.end(), 81) << std::endl;
+    std::cout << std::endl;
+
+    // Test std::find
+    std::cout << "Using std::find:  1 ==> " << builtin_find(values,  1) << std::endl;
+    std::cout << "Using std::find:  3 ==> " << builtin_find(values,  3) << std::endl;
+    std::cout << "Using std::find: 14 ==> " << builtin_find(values, 14) << std::endl;
+    std::cout << "Using std::find: 81 ==> " << builtin_find(values, 81) << std::endl;
+    std::cout << std::endl;
 }
