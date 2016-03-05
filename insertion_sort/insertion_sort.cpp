398639f8dd0e764cb274b69ca89c255ed9178df4
Simon Moore <simon@snmoore.net>
2016-03-05 11:14:20 +0000

    Rewritten for practice

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -1,82 +1,76 @@
-// Sort a vector using insertion sort
-//
-// Insertion sort compares an item against those to the left, maintaining the
-// sorted set on the left and the unsorted set on the right, and iterates until
-// the entire set is sorted
-//
-// Executes in O(n^2) time
 
 #include <algorithm>    // For move
-#include <iostream>     // For cout etc
-#include <vector>       // For vector
+#include <iostream>     // For std:cout etc
+#include <string>       // For std::string
+#include <vector>       // For std::vector
 
-using namespace std;
-
-void print(vector<int> data) {
-    for(auto value : data) {
-        cout << value << " ";
-    }
-    cout << endl;
-}
-
-// Sort the elements, moving elements rightwards one at a time
-void sort(vector<int>& data) {
-    // Work rightwards through the vector, keeping sorted elements on the left
-    for(int j = 1; j < (int)data.size(); j++) {
-        // The value being sorted
+// Insertion sort, moving one element at a time
+void sort_one(std::vector<int>& data) {
+    // Work rightwards from the second element
+    for(int j = 1; j < static_cast<int>(data.size()); j++) {
+        // The current element will be overwritten
         int value = data[j];
 
-        // Compare against values to left to find the insertion point
+        // Sort by comparing the current element to the elements to the left
         int i = j - 1;
-        while((i >= 0) && (value < data[i])) {
-            // Move the larger elements rightwards
-            data[i+1] = data[i];
+        while(i >= 0 && value < data[i]) {
+            // Move the larger element rightwards
+            data[i + 1] = data[i];
             i--;
         }
-        // Insert the value at the insertion point
-        data[i+1] = value;
+        // Move the smaller element leftwards
+        data[i + 1] = value;
     }
 }
 
-// Sort the elements, moving elements rightwards as a block
-void sort_move(vector<int>& data) {
-    // Work rightwards through the vector, keeping sorted elements on the left
-    for(int j = 1; j < (int)data.size(); j++) {
-        // The value being sorted
+// Insertion sort, moving elements as a block
+void sort_move(std::vector<int>& data) {
+    // Work rightwards from the second element
+    for(int j = 1; j < static_cast<int>(data.size()); j++) {
+        // The current element will be overwritten
         int value = data[j];
 
-        // Compare against values to left to find the insertion point
+        // Sort by comparing the current element to the elements to the left
         int i = j - 1;
-        while((i >= 0) && (value < data[i])) {
-            i--;
+        while(i >= 0 && value < data[i]) {
+            --i;
         }
+        // Move the larger elements rightwards
+        std::move(data.begin() + i, data.begin() + j, data.begin() + i + 1);
+        // std::move(&data[i], &data[j], &data[i + 1]);
 
-        // Move the larger elements rightwards as a block
-        move(data.begin()+i, data.begin()+j, data.begin()+i+1);
-
-        // Insert the value at the insertion point
-        data[i+1] = value;
+        // Move the smaller element leftwards
+        data[i + 1] = value;
     }
+}
+
+// Print all the elements of a vector
+void print(const std::vector<int>& data) {
+    for(auto iter : data) {
+        std::cout << iter << " ";
+    }
+    std::cout << std::endl;
+}
+
+// Test an implementation
+void test(void (*function)(std::vector<int>& data), const std::string& label) {
+    std::cout << label << ":" << std::endl;
+
+    std::vector<int> data = { 23, 21, 76, 16, 52, 43 };
+    std::cout << "\tUnsorted: ";
+    print(data);
+
+    function(data);
+    std::cout << "\tSorted:   ";
+    print(data);
+
+    std::cout << std::endl;
 }
 
 int main() {
-    // Start with some unsorted data
-    vector<int> data = { 23, 21, 76, 16, 52, 43 };
-    cout << "Unsorted:" << endl;
-    print(data);
+    // Test insertion sort, moving one element at a time
+    test(sort_one, "Moving one element at a time");
 
-    // Sort the elements, moving elements rightwards one at a time
-    sort(data);
-    cout << endl << "Sorted:" << endl;
-    print(data);
-
-    // Some more unsorted data
-    vector<int> more = { 11, 5, 3, 7, 9, 1 };
-    cout << endl << "Unsorted:" << endl;
-    print(more);
-
-    // Sort the elements, moving elements rightwards one at a time
-    sort_move(more);
-    cout << endl << "Sorted:" << endl;
-    print(more);
+    // Test insertion sort, moving elements as a block
+    test(sort_move, "Moving elements as a block");
 }
